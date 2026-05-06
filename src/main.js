e1182fe325507a3df0c0e9cce994f1890060fb6f
Y.Jhang <MomiziShika599@outlook.com>
2026-05-06 17:29:17 +0800

    style: update social media icons

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const loading = document.getElementById('loading')
+if (loading) {
+  loading.classList.add('done')
+  loading.addEventListener('transitionend', () => loading.remove(), { once: true })
+}
